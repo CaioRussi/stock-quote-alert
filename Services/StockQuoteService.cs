8e93bbadc27859c0d9ee9795d182e8d251e59393
Caio Russi <caio@esferabevi.com.br>
2020-10-13 20:11:57 -0300

    use Refit to create httpClient in StockQuoteService

--- Services/StockQuoteService.cs
+++ Services/StockQuoteService.cs
@@ -4,7 +4,9 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using stock_quote_alert.Abstractions;
 using stock_quote_alert.Configurations;
+using Refit;
 
 namespace stock_quote_alert.Services
 {
@@ -12,12 +14,15 @@ namespace stock_quote_alert.Services
     {
         private readonly ILogger<StockQuoteService> _logger;
         private readonly ServiceConfigurations _serviceConfigurations;
+        private readonly IStockQuoteApi _apiClient;
 
         public StockQuoteService(ILogger<StockQuoteService> logger,
             IOptions<ServiceConfigurations> serviceConfigurations)
         {
             _serviceConfigurations = serviceConfigurations.Value;
             _logger = logger;
+
+            _apiClient = RestService.For<IStockQuoteApi>(_serviceConfigurations.Api.BaseUrl);
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -25,6 +30,8 @@ namespace stock_quote_alert.Services
             while (!stoppingToken.IsCancellationRequested)
             {
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+
+                var response = _apiClient.GetQuote(_serviceConfigurations.Api.ApiKey, "petr4").Result;
 
                 await Task.Delay(TimeSpan.FromMinutes(_serviceConfigurations.Api.IntervalInMinutes), stoppingToken);
             }
